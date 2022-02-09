cd0e432bc00d286edd6dd91cb91a980d061a2ebb
Mackenzel <mackenzielyarnall@gmail.com>
2022-02-09 15:20:17 -0500

    Added Bootstrap
    Installed bootstrap and imported in Index,js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Mackenzie Yarnall and is {""}
+          <a
+            href="https://github.com/Mackenzel/react-weather-forecast"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on github
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
