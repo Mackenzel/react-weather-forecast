1f050387af89ea0871b5c15d71973a5bcdd1a262
Mackenzel <mackenzielyarnall@gmail.com>
2022-02-11 15:44:28 -0500

    Added basic CSS/styling to Weather Component
    information still isnt real but it looks nice.
    2/11/22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by Mackenzie Yarnall and is {""}
           <a
@@ -12,7 +14,7 @@ export default function App() {
             target="_blank"
             rel="noreferrer"
           >
-            open-sourced on github
+            open-sourced on Github
           </a>
           .
         </footer>
